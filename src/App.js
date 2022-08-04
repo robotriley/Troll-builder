9504b3848b7bd005a2847d92d9c4126d16846d30
riley soloner <rileysoloner@gmail.com>
2022-08-04 17:43:10 -0400

    updated header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './components/About.js'
 import NavBar from './components/NavBar.js'
 import GuestBook from './components/GuestBook.js'
+import Header from './components/Header.js'
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <h1>TRoLL SiMULATOR v 1.0 BETA BETA</h1>
+      <Header />
       <NavBar />
       <Routes>
         <Route path="/" element={<GameArea />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
 return (
 <div>
-<h1>Troll Builder</h1>
+    <h1 id="headerText">TRoLL SiMULATOR v 1.0 BETA BETA</h1>
 </div>
 )}
 
